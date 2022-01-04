ca4c230dfc093410400ba2459e6024f7ee49e961
ehrud` <dbzoseh2rl123@naver.com>
2022-01-04 19:09:07 +0900

    10998 번 1008추가

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -1,11 +1,19 @@
 # 10718
-# print("강한친구 대한육군")
-# print("강한친구 대한육군")
+print("강한친구 대한육군")
+print("강한친구 대한육군")
 
 # 1000
-# A, B = map(int, input().split())
-# print(A + B)
+A, B = map(int, input().split())
+print(A + B)
 
 # 1001
-# A, B = map(int, input().split())
-# print(A - B)
+A, B = map(int, input().split())
+print(A - B)
+
+# 10998
+A, B = map(int, input().split())
+print(A * B)
+
+# 1008
+A, B = map(int, input().split())
+print(A / B)
