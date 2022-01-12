8f6589c40b85cc883f73e3b2076c88cda535fd71
ehrud` <dbzoseh2rl123@naver.com>
2022-01-12 22:57:53 +0900

    2440 추가

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -94,3 +94,10 @@ for i in range(1, n + 1):
     for k in range(i):
         print("*", end="")
     print('')
+
+# 2440
+n = int(input())
+for i in range(n):
+    for j in range(n - i):
+        print('*', end="")
+    print()
