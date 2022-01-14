a93e74d2b5f018c49d0fb3d8134b613413d3948f
ehrud` <dbzoseh2rl123@naver.com>
2022-01-14 17:56:21 +0900

    6006, 6007, 6008, 6009, 6010추가

--- code up(python 기초 100제).py
+++ code up(python 기초 100제).py
@@ -12,3 +12,20 @@ print("'Hello'")
 
 # 6005
 print('"Hello World"')
+
+# 6006
+print('"!@#$%^&*()\'')
+
+# 6007
+print("\"C:\Download\\'hello'.py\"")
+
+# 6008
+print('print("Hello\\nWorld")')
+
+# 6009
+n = input()
+print(n)
+
+# 6010
+z = int(input())
+print(z)
