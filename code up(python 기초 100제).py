174221acd65c8d2cb1c20485e6a35b2afd327ec9
이도경 <dbzoseh2rl123@naver.com>
2022-04-03 17:55:07 +0900

    6056 add

--- code up(python 기초 100제).py
+++ code up(python 기초 100제).py
@@ -273,3 +273,9 @@ a, b = map(int, input().split())
 c = bool(int(a))
 d = bool(int(b))
 print((c and (not d)) or ((not c) and d))
+
+# 6057
+a, b = map(int, input().split())
+a = int(a)
+b = int(b)
+print(bool(a) == bool(b))
