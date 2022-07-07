0e700901b302a068cfe86d125bfd54a51f466a07
이도경 <dbzoseh2rl123@naver.com>
2022-07-07 10:54:56 +0900

    6061 add

--- code up(python 기초 100제).py
+++ code up(python 기초 100제).py
@@ -307,3 +307,11 @@ print(x)
 a, b = map(int, input().split())
 c = (a if (a >= b) else b)
 print(c)
+
+
+# 6064
+a, b, c = map(int, input().split())
+f = (a if a < b else b) if ((a if a < b else b) < c)else c
+print(f)
+# 3개중 큰거 구하는 삼항식
+#(a if a > b else b) if ((a if a > b else b) > c) else c
