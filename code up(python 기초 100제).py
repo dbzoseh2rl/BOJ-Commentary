be9e963e201719b8d8c15b487b49121378823c3d
이도경 <dbzoseh2rl123@naver.com>
2022-07-13 13:59:24 +0900

    6061 add

--- code up(python 기초 100제).py
+++ code up(python 기초 100제).py
@@ -324,3 +324,18 @@ if b % 2 == 0:
     print(b)
 if c % 2 == 0:
     print(c)
+
+# 6066
+a, b, c = map(int, input().split())
+if a % 2 == 0:
+    print("even")
+else:
+    print("odd")
+if b % 2 == 0:
+    print("even")
+else:
+    print("odd")
+if c % 2 == 0:
+    print("even")
+else:
+    print("odd")
