fb9e1603aecbabfbba08509fe1bcdce2e28961bc
이도경 <dbzoseh2rl123@naver.com>
2022-07-16 05:30:38 +0900

    6068 add

--- code up(python 기초 100제).py
+++ code up(python 기초 100제).py
@@ -366,3 +366,16 @@ else:
       print('C')
     else:
         print('D')
+
+# 6069
+n = input()
+if n == 'A':
+    print("best!!!")
+elif n == 'B':
+    print("good!!")
+elif n == 'C':
+    print("run!")
+elif n == 'D':
+    print("slowly~")
+else:
+    print("what?")
