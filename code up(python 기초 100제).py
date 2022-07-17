691a39ec019c651eab0dbe0db6953ccd51fac008
이도경 <dbzoseh2rl123@naver.com>
2022-07-17 17:58:36 +0900

    6068 add

--- code up(python 기초 100제).py
+++ code up(python 기초 100제).py
@@ -379,3 +379,14 @@ elif n == 'D':
     print("slowly~")
 else:
     print("what?")
+
+# 6070
+n = int(input())
+if n//3 == 1:
+    print("spring")
+elif n//3 == 2:
+    print("summer")
+elif n//3 == 3:
+    print("fall")
+else:
+    print("winter")
