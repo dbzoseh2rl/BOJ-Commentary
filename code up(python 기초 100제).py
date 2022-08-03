a4b4612d809b8b3d6cf78a270c0ef4e433bdd961
이도경 <dbzoseh2rl123@naver.com>
2022-08-03 20:37:19 +0900

    2751 add

--- code up(python 기초 100제).py
+++ code up(python 기초 100제).py
@@ -426,3 +426,11 @@ for i in range(n + 1):
 n = int(input())
 for i in range(n + 1):
   print(i)
+
+# 6077
+n = int(input())
+s = 0
+for i in range(1, n + 1):
+  if i % 2 == 0:
+    s += i
+print(s)
