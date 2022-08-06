a2ca20bc96fe3587696131549361196d98faee6d
이도경 <dbzoseh2rl123@naver.com>
2022-08-06 22:52:14 +0900

    6081 add

--- code up(python 기초 100제).py
+++ code up(python 기초 100제).py
@@ -447,3 +447,8 @@ n, m = map(int, input().split())
 for i in range(1, n + 1):
   for j in range(1, m + 1):
     print(i, j)
+
+# 6081
+n = int(input(), 16)
+for i in range(1, 16):
+    print('%X*%X=%X' % (n, i, n * i))
