e81f4bdee0e964227eac1b8b1353d67089b6c8be
이도경 <dbzoseh2rl123@naver.com>
2022-08-16 18:05:35 +0900

    6088 add

--- code up(python 기초 100제).py
+++ code up(python 기초 100제).py
@@ -488,3 +488,9 @@ for i in range(1, n + 1):
   if i % 3 == 0:
     continue            #다음 반복 단계로 넘어간다.
   print(i, end=' ')    #i가 짝수가 아닐 때만 실행된다.
+
+# 6088
+a, d, n = map(int, input().split())
+for i in range(1, n):
+    a += d
+print(a)
