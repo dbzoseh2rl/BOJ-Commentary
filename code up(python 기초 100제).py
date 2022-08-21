c321592da36dec22ff34314f4fcfa84b2ab2f890
이도경 <dbzoseh2rl123@naver.com>
2022-08-21 23:29:28 +0900

    6091 add

--- code up(python 기초 100제).py
+++ code up(python 기초 100제).py
@@ -514,3 +514,10 @@ d = 1
 while d % a != 0 or d % b != 0 or d % c !=0:
     d += 1
 print(d)
+
+# 6092
+n = int(input())      #개수를 입력받아 n에 정수로 저장
+a = input().split()  #공백을 기준으로 잘라 a에 순서대로 저장
+
+for i in range(n):  #0부터 n-1까지...
+  a[i] = int(a[i])       #a에 순서대로 저장되어있는 각 값을 정수로 변환해 다시 저장
