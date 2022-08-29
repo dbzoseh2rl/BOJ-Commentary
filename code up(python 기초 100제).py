1514ec9b94733bbebc4fa04a0012f617b43f283c
이도경 <dbzoseh2rl123@naver.com>
2022-08-29 22:30:06 +0900

    6094 add

--- code up(python 기초 100제).py
+++ code up(python 기초 100제).py
@@ -521,3 +521,10 @@ a = input().split()  #공백을 기준으로 잘라 a에 순서대로 저장
 
 for i in range(n):  #0부터 n-1까지...
   a[i] = int(a[i])       #a에 순서대로 저장되어있는 각 값을 정수로 변환해 다시 저장
+
+
+# 6093
+n = int(input())
+a = input().split()
+for i in range(n-1, -1, -1):
+  print(a[i], end=' ')
