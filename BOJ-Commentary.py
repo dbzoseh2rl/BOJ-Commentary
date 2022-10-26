71173fd24a9d4eccff43c6ab262117947d9415c1
lee <dbzoseh2rl123@naver.com>
2022-10-26 22:00:14 +0900

    2442 add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -323,3 +323,12 @@ for i in range(0, n + 1):
     for k in range(n - i):
         print("*", end="")
     print()
+
+# 2442
+n = int(input())
+for i in range(1, n + 1):
+    for j in range(5 - i):
+        print(" ", end="")
+    for k in range(0, 2*i - 1):
+        print("*", end="")
+    print()
