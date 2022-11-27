02cb2a0752f45c47a10618213ee324be053e1ec2
lee <dbzoseh2rl123@naver.com>
2022-11-28 00:20:35 +0900

    2439 other code

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -99,6 +99,11 @@ for i in range(1, n + 1):
     for k in range(i):
         print("*", end="")
     print('')
+# 다시짠거
+# import sys
+# n = int(sys.stdin.readline())
+# for i in range(1, n + 1):
+#     print(" "*(n - i) + "*"*(i))
 
 # 2440
 n = int(input())
