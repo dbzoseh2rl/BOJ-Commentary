fb52166a0128989f9cff3178cbf989ce4b5c3e1e
lee <dbzoseh2rl123@naver.com>
2023-01-11 21:38:17 +0900

    1193 add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -597,3 +597,17 @@ for i in range(t):
     for j in b:
         print(j*int(a), end='')
     print()
+
+# 1193
+n = int(input())
+line = 1
+while n > line:
+    n -= line
+    line += 1
+if line % 2 == 0:
+    up = n
+    down = line - n + 1
+else:
+    up = line - n + 1
+    down = n
+print(f'{up}/{down}')
