9acd0f103ba9dd276907c4cb20ccb560fc369490
lee <dbzoseh2rl123@naver.com>
2023-01-14 20:55:24 +0900

    재귀구현 n! add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -630,7 +630,7 @@ def recursive_dfs(v, discovered = [] ):
             discovered = recursive_dfs(w, discovered)
             return discovered
 
-재귀 예제 2
+# 재귀 예제 2
 # 반복적으로 구현한 n!
 def factorial_iterative(n):
     result = 1
@@ -638,3 +638,15 @@ def factorial_iterative(n):
     for i in range(1, n + 1):
        result *= i
     return result
+
+# 재귀적으로 구현한 n!
+def factorial_recursive(n):
+    if n <= 1: # n이 1 이하인 경우 1을 반환
+        return 1
+    # n! = n * (n - 1)!를 그대로 코드로 작성하기
+    return n * factorial_recursive(n - 1)
+
+# 각각의 방식으로 구현한 n! 출력(n = 5)
+print('반복적으로 구현:', factorial_iterative(5))
+print('재귀적으로 구현:', factorial_recursive(5))
+
