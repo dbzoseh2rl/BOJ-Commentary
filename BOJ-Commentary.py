8a617d0f7d7a673c725f65a83b1a7d485e48f500
lee <dbzoseh2rl123@naver.com>
2023-01-27 15:33:23 +0900

    9498 add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -699,3 +699,16 @@ elif A < B:
     print('<')
 elif A == B:
     print('==')
+
+# 9498
+a = int(input())
+if a >= 90:
+    print('A')
+elif a >= 80:
+    print('B')
+elif a >= 70:
+    print('C')
+elif a >= 60:
+    print('D')
+else:
+    print('F')
