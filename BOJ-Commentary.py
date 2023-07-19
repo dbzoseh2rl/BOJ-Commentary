01247a1379fd74c0ddb09d48364dc25294a4dd48
ehrud <dbzoseh2rl123@naver.com>
2023-07-19 17:39:24 +0900

    9654 add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -952,3 +952,10 @@ print(cnt)
 # 막대기 길이 전부 이진수로 표현 가능하기때문에
 # print(bin(int(input())).count("1"))
 
+# 9654
+print("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE")
+print("N2 Bomber      Heavy Fighter  Limited    21        ")
+print("J-Type 327     Light Combat   Unlimited  1         ")
+print("NX Cruiser     Medium Fighter Limited    18        ")
+print("N1 Starfighter Medium Fighter Unlimited  25        ")
+print("Royal Cruiser  Light Combat   Limited    4         ")
