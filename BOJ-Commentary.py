0ade0a1d2282ccb42cd46c7534631eab0521d273
ehrud <dbzoseh2rl123@naver.com>
2023-09-17 18:41:01 +0900

    2480 add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -1125,3 +1125,14 @@ if m >= 60:
 if h >= 24:
     h -= 24
 print(h, m)
+
+# 2480
+a, b, c = map(int, input().split())
+if a == b == c:
+    print(10000 + a * 1000)
+elif a == b or a == c:
+    print(1000 + a * 100)
+elif b == c:
+    print(1000 + b * 100)
+else:
+    print(max(a, b, c) * 100)
