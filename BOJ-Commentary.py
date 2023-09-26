ee08d7f563ab968e7403398fd7bf7f3fb97c74fd
ehrud <dbzoseh2rl123@naver.com>
2023-09-27 00:07:45 +0900

    1978 add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -1195,3 +1195,18 @@ print(ans)
 a = input()
 mbti = [input() for _ in range(int(input()))]
 print(mbti.count(a))
+
+# 1978
+n = int(input())
+data = map(int, input().split())
+cnt = 0
+for i in data:
+    check = 0
+    if i == 1:
+        continue
+    for j in range(2, i):
+        if i % j == 0:
+            check += 1
+    if check == 0:
+        cnt += 1
+print(cnt)
