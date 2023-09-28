6ee4cf278134ac47185e25565ef64bbf75cbdf80
THSD <dbzoseh2rl123@naver.com>
2023-09-28 12:46:39 +0900

    1408 add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -1210,3 +1210,16 @@ for i in data:
     if check == 0:
         cnt += 1
 print(cnt)
+
+# 1408
+hour1, min1, sec1 = map(int, input().split(":"))
+hour2, min2, sec2 = map(int, input().split(":"))
+total = (hour2*3600 + min2*60 + sec2) - (hour1*3600 + min1*60 + sec1)
+
+if total < 0:
+    total += 60*60*24
+
+real_hour = total // 3600
+real_min = (total%3600)//60
+real_sec = total%60
+print("%02d:%02d:%02d" % (real_hour,real_min,real_sec))
