60eb6bb11513ed1e1de5f7fbce056cfa11238fb3
ehrud <dbzoseh2rl123@naver.com>
2023-10-02 18:46:07 +0900

    2712 add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -1210,3 +1210,15 @@ for i in data:
     if check == 0:
         cnt += 1
 print(cnt)
+
+# 2712
+for _ in range(int(input())):
+    n, s = input().split()
+    if s == "kg":
+        print("%.4f %s" % (float(n)*2.2046, "lb"))
+    elif s == "lb":
+        print("%.4f %s" % (float(n)*0.4536, "kg"))
+    elif s == "l":
+        print("%.4f %s" % (float(n)*0.2642, "g"))
+    elif s == "g":
+        print("%.4f %s" % (float(n)*3.7854, "l"))
