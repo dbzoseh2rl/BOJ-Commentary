05285373b8805b851493a6ba406295050761ea38
dbzoseh2rl <dbzoseh2rl123@naver.com>
2023-12-03 23:40:12 +0900

    # 1181  add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -1557,3 +1557,15 @@ D = int(input())
 x = (A / C)
 y = (B / D)
 print(int(l - max(x, y)))
+
+# 1181
+n = int(input())
+s = []
+for _ in range(n):
+    s.append(input())
+s = list(set(s))
+s.sort()
+s.sort(key=len)
+
+for i in s:
+    print(i)
