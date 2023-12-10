5ca427a5cf98879e62e4f9d04e703b449a52169e
dbzoseh2rl <dbzoseh2rl123@naver.com>
2023-12-10 15:40:09 +0900

    # 10989 add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -1586,3 +1586,14 @@ for i in range(n):
     a = (a % b) * 10
     result = a // b
 print(result)
+
+# 10989
+import sys
+n = int(sys.stdin.readline())
+b = [0] * 10001
+for i in range(n):
+    b[int(sys.stdin.readline())] += 1   #입력값과 같은 인덱스에 +1
+for i in range(10001):
+    if b[i] != 0:
+        for j in range(b[i]):
+            print(i)
