625ce5478e2f0098bb01a8974f0a1f8b92902211
dbzoseh2rl <dbzoseh2rl123@naver.com>
2023-12-20 21:54:08 +0900

    # 1417   add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -1655,3 +1655,11 @@ else:
     cnt += 1
     other.sort(reverse=True)
   print(cnt)
+
+import sys
+h, m, s = map(int, sys.stdin.readline().split())
+t = int(sys.stdin.readline())
+s += t
+m += s//60
+h += m//60
+print(h % 24, m % 60, s % 60)
