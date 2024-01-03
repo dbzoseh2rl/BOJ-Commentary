b98c46006fc836cd9e3a9f623272bad476d7b8cb
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-01-03 20:19:38 +0900

    # 26711 add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -1728,3 +1728,15 @@ for i in range(1, n):
     print(" "*(n-i) + "*"*(2*i - 1))
 for i in range(n, 0, -1):
     print(" "*(n-i) + "*"*(2*i - 1))
+
+
+# 26711
+def A_plus_B(A, B):
+    return A + B
+
+
+if __name__ == "__main__":
+    A = int(input())
+    B = int(input())
+
+    print(A_plus_B(A=A, B=B))
