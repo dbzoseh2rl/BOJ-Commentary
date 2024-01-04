1aa6a01fe08d6509b5db82b6b25bb6853f967c19
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-01-04 22:49:12 +0900

    # 2349 add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -1754,3 +1754,9 @@ import sys
 n = int(sys.stdin.readline())
 for i in range(0, n + 1):
     print(" "*i + "*"*(n - i))
+
+# 2349
+import sys
+n = int(sys.stdin.readline())
+for i in range(1, n + 1):
+    print(" "*(n - i) + "*"*(i))
