8e90b3de8e216aeac545ac73d11f1b4663dc6fa9
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-01-14 10:00:00 +0900

    # 1075 add

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -1822,3 +1822,13 @@ while True:
         print("Yes")
     else:
         print("No")
+
+# 1075
+n = input()
+f = int(input())
+a = int(n[:-2] + '00')
+while True:
+    if a % f == 0:
+        break
+    a += 1
+print(str(a)[-2:])
