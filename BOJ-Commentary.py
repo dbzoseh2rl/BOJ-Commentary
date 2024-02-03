f49c49b4c5f616a3381fd9e1680e5ec93d3bbfb0
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-02-03 16:46:56 +0900

    # 1100  ADD

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -1924,3 +1924,12 @@ print("".join(sorted(list(input()))[::-1]))
 
 # nums = map(int, input().split())
 # print(sum(sorted(nums)[::2]))
+
+# # 1100
+# chess = [input() for _ in range(8)]
+# cnt = 0
+# for x in range(8):
+#     for y in range(8):
+#         if y % 2 == x % 2 and chess[x][y] == 'F':
+#             cnt += 1
+# print(cnt)
