807070d561671781521cd1fa66c5a3c4692ddacb
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-02-07 21:01:26 +0900

    # 1439  ADD

--- BOJ-Commentary.py
+++ BOJ-Commentary.py
@@ -1953,3 +1953,16 @@ print("".join(sorted(list(input()))[::-1]))
 #         res.append(c)
 # # print(res)
 # print(''.join(sorted(res)) if len(res) > 0 else "PREDAJA")
+
+# # 1439
+# import sys
+# s = list(map(int, sys.stdin.readline().strip()))
+# cnt = 0
+# # 반복문을 통해 문자열을 확인
+# for i in range(len(s) - 1):
+#     # 현재 문자와 다음 문자가 다르면 카운트
+#     if s[i] != s[i + 1]:
+#         cnt += 1
+# # print(cnt)
+# # cnt + 1을 2로 나눈 나머지를 출력
+# print((cnt + 1) // 2)
